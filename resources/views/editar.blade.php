bca4016fa79c4f3eb3ae61e702d671636486363b
EnzoCostaPaz <133404019+EnzoCostaPaz@users.noreply.github.com>
2025-04-06 23:24:12 -0300

    Adição de estilização da tela de editar

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -57,4 +57,130 @@
     </form>
 </body>
 
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@400;600&display=swap');
+
+    /* Estilização do body*/
+    body {
+        font-family: 'Kanit', sans-serif;
+        background-color: #f8f9fa;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100vh;
+    }
+
+    /* Estilização do menu*/
+
+    nav {
+        width: 100%;
+        background-color: #007bff;
+        padding: 10px 20px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1000;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .Titulo {
+        font-size: 24px;
+        font-weight: bold;
+        color: white;
+    }
+
+    .menu {
+        list-style: none;
+        display: flex;
+        padding: 0;
+        margin: 0;
+    }
+
+    .menu li {
+        margin: 0 15px;
+    }
+
+    .menu li a {
+        color: white;
+        text-decoration: none;
+        padding: 10px 15px;
+        font-weight: bold;
+        border-radius: 4px;
+        transition: background-color 0.3s;
+    }
+
+    .menu li a:hover {
+        background-color: #0056b3;
+        text-shadow: 0px 0px 5px #fff;
+    }
+
+    .Label-Inform {
+        margin-bottom: 90px;
+        font-size: 30px;
+        font-weight: bold;
+    }
+
+    .container {
+        margin-top: 65px;
+    }
+
+    /*estilização do forms */
+    form {
+        background-color: #ffffff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+        width: 38.75rem;
+        margin-top: 30px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .container_nome,
+    .container_telefone,
+    .container_observacoes,
+    .container_Origens,
+    .container_Data {
+        display: flex;
+        flex-direction: column;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
+
+    input,
+    select,
+    textarea {
+        padding: 8px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        font-size: 14px;
+        font-family: inherit;
+    }
+
+    button {
+        background-color: #007bff;
+        color: white;
+        border: none;
+        padding: 10px;
+        font-size: 16px;
+        width: 100%;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: background 0.3s;
+    }
+
+    button:hover {
+        background-color: #0056b3;
+    }
+</style>
+
 </html>
