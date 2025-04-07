22b8f76644c515a75010bec9f7d8b115a3dbe1fe
EnzoCostaPaz <133404019+EnzoCostaPaz@users.noreply.github.com>
2025-04-06 23:15:52 -0300

    padronização na tela de editar

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -23,26 +23,36 @@
 
     <form action="/editar_cliente/{{ $cliente->id}}" method="post">
         @csrf
-        <label for="Nome_Label" class="Nome_Label">Nome:</label>
-        <input type="text" name="Nome_Input" value="{{$cliente->Nome_Input}}">
-        <br><br>
-        <label for="Telefone_Label" class="Telefone_Label">Telefone:</label>
-        <input type="text" name="Telefone_Input" value="{{$cliente->Telefone_Input}}">
-        <br><br>
-        <label for="Select_Origens">Origem:</label>
-        <br>
-        <select name="Select_Origens" required>
-            <option value="Celular" {{ $cliente->Select_Origens == 'Celular' ? 'selected' : '' }}>Celular</option>
-            <option value="Telefone-Fixo" {{ $cliente->Select_Origens == 'Telefone-Fixo' ? 'selected' : '' }}>Telefone-Fixo</option>
-            <option value="Whatsapp" {{ $cliente->Select_Origens == 'Whatsapp' ? 'selected' : '' }}>Whatsapp</option>
-        </select>
-        <br><br>
-        <label class="Data_Label">Data de Contato</label>
-        <input type="date" class="Data_Input" name="Data_Input" value = "{{$cliente-> Data_Input}}">
-        <br><br>
-        <label for="Oberserve_Input">Observações:</label>
-        <input type="text" name="Oberserve_Input" value="{{ $cliente->Oberserve_Input }}">
-        <br><br>
+        <div class="conatainer_nome">
+            <label for="Nome_Label" class="Nome_Label">Nome:</label>
+            <input type="text" name="Nome_Input" value="{{$cliente->Nome_Input}}">
+        </div>
+
+        <div class="container_telefone">
+            <label for="Telefone_Label" class="Telefone_Label">Telefone:</label>
+            <input type="text" name="Telefone_Input" value="{{$cliente->Telefone_Input}}">
+        </div>
+
+        <div class="container_Origens">
+            <label for="Select_Origens">Origem:</label>
+            <br>
+            <select name="Select_Origens" required>
+                <option value="Celular" {{ $cliente->Select_Origens == 'Celular' ? 'selected' : '' }}>Celular</option>
+                <option value="Telefone-Fixo" {{ $cliente->Select_Origens == 'Telefone-Fixo' ? 'selected' : '' }}>Telefone-Fixo</option>
+                <option value="Whatsapp" {{ $cliente->Select_Origens == 'Whatsapp' ? 'selected' : '' }}>Whatsapp</option>
+            </select>
+        </div>
+
+        <div class="container_Data">
+            <label class="Data_Label">Data de Contato</label>
+            <input type="date" class="Data_Input" name="Data_Input" value="{{$cliente-> Data_Input}}">
+        </div>
+
+        <div class="container_observacoes">
+            <label for="Oberserve_Input">Observações:</label>
+            <input type="text" name="Oberserve_Input" value="{{ $cliente->Oberserve_Input }}">
+        </div>
+
         <button>Editar</button>
     </form>
 </body>
